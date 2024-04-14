1e51e0b7520dc4eb0a6f8da2d41c150f16685545
El Abror <luxamrownn@gmail.com>
2024-04-14 13:49:22 +0700

    update

--- src/data/resume-data.tsx
+++ src/data/resume-data.tsx
@@ -56,7 +56,7 @@ export const RESUME_DATA = {
       start: "2022",
       end: "Now",
       description:
-        "Develop new feature and services based on requirements, develop client side logic, implemented of REST API, slicing design from Figma design",
+        "Research, innovate and develop new feature and services based on requirements, develop client side logic, implemented of REST API, slicing design from Figma design",
     }
   ],
   organization: [
